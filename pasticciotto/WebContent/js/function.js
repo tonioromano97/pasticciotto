fd9eaf5263f79a9626f01466d815ef6005438e86
giuliova97 <gvarone.10@gmail.com>
2019-01-18 15:03:50 +0100

    Revision implementation

---
+++ pasticciotto/WebContent/js/function.js
@@ -0,0 +1,26 @@
+function viewProducts(){
+	document.getElementById("information").style.display = "table";
+	var div = document.getElementById("viewInformationOrProducts");
+	div.innerHTML = "" +
+			"<table class=\"table\">" +
+			"<thead>" +
+			"<tr>" +
+			"<th> Nome </th>" +
+			"<th> Prezzo </th>" +
+			"<th> Prenota </th>" +
+			"</tr>" +
+			"</thead>" +
+			"<tbody>" +
+			"<tr>" +
+			"<td> Ricotta e Pera </td>" +
+			"<td> &euro; 10.00 /kg </td>" +
+			"<td> <button style=\"float:left;\" class=\"btn btn-primary\"> &euro; Prenota</button> </td>" +
+			"</tr>" +
+			"<tr>" +
+			"<td> Panna e Nutella</td>" +
+			"<td> &euro; 8.00 /kg </td>" +
+			"<td> <button style=\"float:left;\" class=\"btn btn-primary\"> &euro; Prenota</button> </td>" +
+			"</tr>" +
+			"</tbody>" +
+			"</table>";
+}
