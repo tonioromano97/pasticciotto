3c1e30d4f96ff8106207972ef3e37d8801eb0970
giuliova97 <gvarone.10@gmail.com>
2019-01-18 15:52:12 +0100

    Added model and Login/Register control

--- pasticciotto/src/bean/Dipendente.java
+++
@@ -1,7 +0,0 @@
-package bean;
-
-public class Dipendente extends Utente {
-	public Dipendente(String nome, String cognome, String email, String pw, String telefono){
-		super(nome,cognome,email,pw,telefono);
-	}
-}
---
+++ pasticciotto/src/control/logincontrol/LoginControl.java
@@ -0,0 +1,54 @@
+package control.logincontrol;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.UserManager;
+
+/**
+ * Servlet implementation class LoginControl
+ */
+@WebServlet("/Login")
+public class LoginControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginControl() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		UserManager manager = new UserManager();
+		try {
+			manager.login(email, password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
