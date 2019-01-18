9b1eb37e9aa9135e977610fc2037185b54481eac
Antonio Romano <tonioromano97@gmail.com>
2019-01-18 14:40:49 +0100

    Add project implementation

---
+++ Project/Pasticciotto/src/bean/Ricetta.java
@@ -0,0 +1,53 @@
+package bean;
+
+import java.util.ArrayList;
+
+public class Ricetta {
+	public Ricetta(String nome){
+		this.nome=nome;
+		this.composizione = new ArrayList<Prodotto>();
+	}
+	public Ricetta(String nome,long tempoMedio,double prezzoVendita,double prezzoAcquisto){
+		this.nome=nome;
+		this.tempoMedio=tempoMedio;
+		this.prezzoAcquisto=prezzoAcquisto;
+		this.prezzoVendita=prezzoVendita;
+		this.composizione = new ArrayList<Prodotto>();
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public long getTempoMedio() {
+		return tempoMedio;
+	}
+	public void setTempoMedio(long tempoMedio) {
+		if(tempoMedio<0) throw new IllegalArgumentException("Tempo medio ricetta minore di 0");
+		this.tempoMedio = tempoMedio;
+	}
+	public double getPrezzoVendita() {
+		return prezzoVendita;
+	}
+	public void setPrezzoVendita(double prezzoVendita) {
+		if(prezzoVendita<0) throw new IllegalArgumentException("Prezzo vendita minore di 0");
+		this.prezzoVendita = prezzoVendita;
+	}
+	public double getPrezzoAcquisto() {
+		return prezzoAcquisto;
+	}
+	public void setPrezzoAcquisto(double prezzoAcquisto) {
+		if(prezzoAcquisto<0) throw new IllegalArgumentException("Prezzo acquisto minore di 0");
+		this.prezzoAcquisto = prezzoAcquisto;
+	}
+	public ArrayList<Prodotto> getComposizione() {
+		return composizione;
+	}
+	
+	private ArrayList<Prodotto> composizione;
+	private String nome;
+	private long tempoMedio;
+	double prezzoVendita,prezzoAcquisto;
+}
