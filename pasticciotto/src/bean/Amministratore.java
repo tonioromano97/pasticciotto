73e94ae24787ecd5c6b979989ef1be079565e439
Antonio Romano <tonioromano97@gmail.com>
2019-01-18 15:33:16 +0100

    Update class

--- pasticciotto/src/bean/Amministratore.java
+++
@@ -1,7 +0,0 @@
-package bean;
-
-public class Amministratore extends Utente {
-	public Amministratore(String nome, String cognome, String email, String pw, String telefono){
-		super(nome,cognome,email,pw,telefono);
-	}
-}
